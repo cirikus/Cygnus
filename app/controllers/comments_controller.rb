2458b7c8c96c6bd566b1a87e17eeb5205392ff38
Cirikus <sebastien.salomon@gmail.com>
2017-03-30 16:52:00 +0200

    Add possibility to del comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,35 @@
 class CommentsController < ApplicationController
 
 	def create
-    	@task = Task.find params[:task_id]
-    	@comment = @task.comments.new(comment_params)
-    	if @comment.save
-      		flash[:notice] = "Successfully created comment!"
-    	else
+    @task = Task.find params[:task_id]
+    @comment = @task.comments.new(comment_params)
+    if @comment.save
+    	flash[:notice] = "Successfully created comment!"
+    else
 			flash[:alert] = "Error creating new comment!"
-    	end
-     	redirect_to task_path(@task)     
-  	end
+    end
+    redirect_to task_path(@task)     
+  end
 
-  	def comment_params
-    	params.require(:comment).permit(:content)
-  	end
+# The destroy action removes the task permanently from the database
+  def destroy
+    @task = Task.find params[:task_id]
+    @comment = @task.comments.find(params[:id])
+    if @comment.destroy
+      flash[:notice] = "Successfully deleted comment!"
+      redirect_to task_path(@task)
+    else
+      flash[:alert] = "Error updating comment!"
+    end
+  end
 
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+  def find_comment
+    @comment = Comment.find(params[:id])
+  end 
 end
